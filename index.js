66c5c32919da21c7426f66c3e5af35abbf116872
Dhiraj139 <87509030+Dhiraj139@users.noreply.github.com>
2022-02-21 12:01:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var img = document.querySelector(".img1");
 if (randomNumber1 === 1) {
-  img.setAttribute("src", "/images/dice1.png");
+  img.setAttribute("src", "https://i.postimg.cc/WbWJfKhY/dice1.png");
 } else if (randomNumber1 === 2) {
-  img.setAttribute("src", "/images/dice2.png");
+  img.setAttribute("src", "https://i.postimg.cc/zXXg75XR/dice2.png");
 } else if (randomNumber1 === 3) {
-  img.setAttribute("src", "/images/dice3.png");
+  img.setAttribute("src", "https://i.postimg.cc/SQG9cjqh/dice3.png");
 } else if (randomNumber1 === 4) {
-  img.setAttribute("src", "/images/dice4.png");
+  img.setAttribute("src", "https://i.postimg.cc/t4sV056q/dice4.png");
 } else if (randomNumber1 === 5) {
-  img.setAttribute("src", "/images/dice5.png");
+  img.setAttribute("src", "https://i.postimg.cc/PqZpJ5K0/dice5.png");
 } else if (randomNumber1 === 6) {
-  img.setAttribute("src", "/images/dice6.png");
+  img.setAttribute("src", "https://i.postimg.cc/vBs1VwNT/dice6.png");
 }
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var img = document.querySelector(".img2");
 if (randomNumber2 === 1) {
-  img.setAttribute("src", "/images/dice1.png");
+  img.setAttribute("src", "https://i.postimg.cc/WbWJfKhY/dice1.png");
 } else if (randomNumber2 === 2) {
-  img.setAttribute("src", "/images/dice2.png");
+  img.setAttribute("src", "https://i.postimg.cc/zXXg75XR/dice2.png");
 } else if (randomNumber2 === 3) {
-  img.setAttribute("src", "/images/dice3.png");
+  img.setAttribute("src", "https://i.postimg.cc/SQG9cjqh/dice3.png");
 } else if (randomNumber2 === 4) {
-  img.setAttribute("src", "/images/dice4.png");
+  img.setAttribute("src", "https://i.postimg.cc/t4sV056q/dice4.png");
 } else if (randomNumber2 === 5) {
-  img.setAttribute("src", "/images/dice5.png");
+  img.setAttribute("src", "https://i.postimg.cc/PqZpJ5K0/dice5.png");
 } else if (randomNumber2 === 6) {
-  img.setAttribute("src", "/images/dice6.png");
+  img.setAttribute("src", "https://i.postimg.cc/vBs1VwNT/dice6.png");
 }
 
 var title = document.querySelector(".title");
